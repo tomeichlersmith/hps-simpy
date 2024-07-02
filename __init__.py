3fda6bb9f88d94e92ec6c89a6a91d98b2afe7e3f
tomeichlersmith <eichl008@umn.edu>
2024-07-02 10:06:04 -0700

    use fully qualified domain name in host check

--- __init__.py
+++ __init__.py
@@ -9,13 +9,13 @@ from pathlib import Path
 
 def get_data_dir():
     import socket
-    hn = socket.gethostname()
+    hn = socket.getfqdn()
     if hn.endswith('slac.stanford.edu'):
         return Path('/sdf/group/hps/user-data/eichl008/simp-l1l2/analysis')
     elif hn.endswith('zebra01.spa.umn.edu'):
         return Path('/export/scratch/users/eichl008/hps/simp-l1l2/data')
     else:
-        raise ValueError('Unrecognized host {hn}')
+        raise ValueError(f'Unrecognized host {hn}')
 
 
 def full_sample_list():
