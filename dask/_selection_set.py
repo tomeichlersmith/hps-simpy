67d2c1c3cdf6136a5cc5564863e5844c441d1f22
tomeichlersmith <eichl008@umn.edu>
2024-07-08 11:00:23 -0700

    patch getattr of SelectionSEt

--- dask/_selection_set.py
+++ dask/_selection_set.py
@@ -12,11 +12,16 @@ class SelectionSet:
     
     def __getattr__(self, name):
         if name in self._aliases:
-            return functools.reduce(operator.and_, (self._selections[n] for n in self._alises[name]))
+            return functools.reduce(
+                operator.and_,
+                (self._selections[n] for n in self._aliases[name])
+            )
         elif name in self._selections:
             return self._selections[name]
+        elif name in self.__dict__:
+            return self.__dict__[name]
         else:
-            return super().__getattr__(self, name)
+            raise ValueError(f'SelectionSet object has no attribute {name}')
 
 
     def __call__(self, *names):
