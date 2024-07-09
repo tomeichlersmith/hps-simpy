cf7024b4d9d2b8acfefd19bdd448f69045fa11ad
tomeichlersmith <eichl008@umn.edu>
2024-07-09 09:24:56 -0500

    drop zebra01 from options, no need for it atm

--- _data_dir.py
+++ _data_dir.py
@@ -7,7 +7,5 @@ def get_data_dir():
     hn = socket.getfqdn()
     if hn.endswith('slac.stanford.edu'):
         return Path('/sdf/group/hps/user-data/eichl008/simp-l1l2/analysis')
-    elif hn.endswith('zebra01.spa.umn.edu'):
-        return Path('/export/scratch/users/eichl008/hps/simp-l1l2/data')
     else:
         raise ValueError(f'Unrecognized host {hn}')
