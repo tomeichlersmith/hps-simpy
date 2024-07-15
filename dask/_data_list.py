26d284e12f21526605c265766b6b682e35c5f5c2
tomeichlersmith <eichl008@umn.edu>
2024-07-15 09:38:18 -0700

    allow downstream user to define data sample

--- dask/_data_list.py
+++ dask/_data_list.py
@@ -17,14 +17,36 @@ def _test_sample_list():
     ]
 
 
-def _full_sample_list():
+def _is_data(fp):
+    return fp.stem.startswith('data-')
+
+
+def _data_10pct(fp):
+    return _is_data(fp) and fp.stem.endswith('part-0')
+
+
+def _golden_run(fp):
+    return _is_data(fp) and 'run-007800' in fp.stem
+
+
+_data_filters = {
+    'is-data' : _is_data,
+    '10pct' : _data_10pct,
+    'golden-run': _golden_run
+}
+
+def _full_sample_list(data_filter = None):
     data_d = get_data_dir()
+    if data_filter is None:
+        data_filter = _is_data
+    elif isinstance(data_filter, str):
+        data_filter = _data_filters[data_filter]
     from collections import defaultdict
     samples = defaultdict(dict)
     for fp in data_d.iterdir():
         if fp.suffix != '.root':
             continue
-        if fp.stem.startswith('data-'):
+        if data_filter(fp):
             samples['data'][fp] = 'preselection'
         elif fp.stem.endswith('simp-beam'):
             m = fp.stem.split('_')[1]
@@ -40,7 +62,7 @@ def _full_sample_list():
     return samples.items()
 
 
-def sample_list(*, test = True):
+def sample_list(*, test = True, data_filter = None):
     if test:
         return _test_sample_list()
     else:
