3c0415c23620b8e7d958d8ac9ec484e78381a678
tomeichlersmith <eichl008@umn.edu>
2024-08-06 16:15:55 -0500

    fixup signal yield to work

--- _signal_yield.py
+++ _signal_yield.py
@@ -24,15 +24,15 @@ def signal_yield(*,
     eff_wf = final_selection_eff_h.values(flow=True)
     eff = eff_wf[1:-1,...]
 
-    mean_gamma = mean_energy_GeV_by_mass[m]*1000 / m
-    decay_gct_eps2_rho = mean_gamma*ctau(model.rate_Vd_decay_2l_eps2(m, rho=True))
-    decay_gct_eps2_phi = mean_gamma*ctau(model.rate_Vd_decay_2l_eps2(m, rho=False))
+    mean_gamma = mean_energy_GeV*1000 / mass
+    decay_gct_eps2_rho = mean_gamma*ctau(model.rate_Vd_decay_2l_eps2(mass, rho=True))
+    decay_gct_eps2_phi = mean_gamma*ctau(model.rate_Vd_decay_2l_eps2(mass, rho=False))
     decay_weight = (
-        model.br(model.rate_Vrho_pi, m)*eps2/decay_gct_eps2_rho*np.exp(
+        model.br(model.rate_Vrho_pi, mass)*eps2/decay_gct_eps2_rho*np.exp(
             np.multiply.outer(-4.3-z.centers, eps2)/decay_gct_eps2_rho
         )
         +
-        model.br(model.rate_Vphi_pi, m)*eps2/decay_gct_eps2_phi*np.exp(
+        model.br(model.rate_Vphi_pi, mass)*eps2/decay_gct_eps2_phi*np.exp(
             np.multiply.outer(-4.3-z.centers, eps2)/decay_gct_eps2_phi
         )
     )
