ef5fe3dbc526dfa7c680d2334920cc03fb84523d
tomeichlersmith <eichl008@umn.edu>
2024-08-06 14:41:24 -0500

    working on calculating signal yield one mass at a time

--- _exclusion_estimate.py
+++ _exclusion_estimate.py
@@ -8,6 +8,8 @@ from tqdm import tqdm
 import hist
 
 from .exclusion import optimum_interval_method as oim
+from .exclusion import production
+from .exclusion.models import ctau, simp
 from ._signal_yield import signal_yield
 
 
@@ -29,29 +31,55 @@ def construct_yield_cdf_lut(differential_yield_distribution):
 
 
 def exclusion_estimate(*,
+    mass,
+    eps2,
+    z,
+    invm_cr_h,
+    final_selection_eff_by_mass_h,
+    mean_energy_GeV_by_mass,
     data_z,
-    **signal_yield_kw
+    simp_parameters = {},
 ):
-    sy = signal_yield(**signal_yield_kw)
     _oim_table = oim.load_or_new(
         max_signal_strength = 50.0,
         n_test_mu = 200,
         n_trials = 10_000
     )
+    total_prompt_signal_yield_per_eps2 =  production.from_calculators(
+        production.radiative_fraction.alic_2016_simps,
+        production.TridentDifferentialProduction.from_hist(invm_cr_h),
+        production.radiative_acceptance.alic_2016_simps
+    )
+    model = simp.Parameters(**simp_parameters)
+    diff_yield = np.full(mass.shape+eps2.shape+z.centers.shape, np.nan)
     max_signal_allowed = np.full(mass.shape+eps2.shape, np.nan)
+    expected_signal = np.full(mass.shape+eps2.shape, 0.0)
     for i_mass, m in tqdm(enumerate(mass), total=len(mass)):
-        yield_cdf_lut = construct_yield_cdf_lut(sy.diff_yield[i_mass,...])
+        diff_yield[i_mass,...] = signal_yield(
+            mass = m,
+            eps2 = eps2,
+            z = z,
+            prompt_signal_yield_per_eps2 = (
+                total_prompt_signal_yield_per_eps2(m*model.mass_ratio_Ap_to_Vd)
+            ),
+            final_selection_eff_h = final_selection_eff_by_mass_h[m],
+            mean_energy_GeV = mean_energy_GeV_by_mass[m],
+            model = model
+        )
+        yield_cdf_lut = construct_yield_cdf_lut(diff_yield[i_mass,...])
+        expected_signal[i_mass,:] = np.sum(diff_yield[i_mass,...], axis=1)
         data_x = (
             yield_cdf_lut[..., z.index(data_z[m])]
             if len(data_z[m]) > 0 else np.full((*yield_cdf_lut.shape[:-1],0), 0.0)
         )
-        max_signal_allowed[i_mass,:] = oim.max_signal_strength_allowed(
-            data_x,
-            confidence_level=0.9
-        )
+        max_signal_allowed[i_mass,:] = oim.max_signal_strength_allowed(data_x, confidence_level=0.9)
 
     return ExclusionEstimateResult(
-        *sy,
-        max_allowed = max_signal_allowed,
+        mass = mass,
+        eps2 = eps2,
+        z = z.centers,
+        diff_yield = diff_yield,
+        expected = expected_signal,
+        max_allowed = max_signal_allowed
     )
 
