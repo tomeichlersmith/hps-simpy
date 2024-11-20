95f224f5dfb36d93effa87995c3fd8a57315c977
tomeichlersmith <eichl008@umn.edu>
2024-11-20 11:50:01 -0800

    support denvs on sdfiana

--- _data_dir.py
+++ _data_dir.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def get_data_dir():
     import socket
     hn = socket.getfqdn()
-    if hn.endswith('slac.stanford.edu'):
+    if hn.endswith('slac.stanford.edu') or 'sdfiana' in hn:
         return Path('/sdf/group/hps/user-data/eichl008/simp-l1l2/analysis')
     elif hn.endswith('zebra01.spa.umn.edu'):
         return Path('/export/scratch/users/eichl008/hps/simp-l1l2/data')
