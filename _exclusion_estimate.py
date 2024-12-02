3bfee55d488d4864ea781bf757b5797e66457eff
tomeichlersmith <eichl008@umn.edu>
2024-12-02 12:06:46 -0800

    separate OIM from differential yield calculation

--- _exclusion_estimate.py
+++ _exclusion_estimate.py
@@ -31,15 +31,13 @@ def construct_yield_cdf_lut(differential_yield_distribution):
     return yield_cdf_lut
 
 
-def exclusion_estimate(*,
+def exclusion_estimate_from_diff_yield(*,
     mass,
     eps2,
     z,
-    invm_cr_h,
-    final_selection_counts_by_mass_h,
-    mean_energy_GeV_by_mass,
+    diff_yield,
     data_z,
-    simp_parameters = {},
+    eff = None
 ):
     _oim_table = oim.load_or_new(
         mu_values = np.concatenate([
@@ -48,28 +46,9 @@ def exclusion_estimate(*,
         ]),
         n_trials = 10_000
     )
-    total_prompt_signal_yield_per_eps2 =  production.from_calculators(
-        production.radiative_fraction.alic_2016_simps,
-        production.TridentDifferentialProduction.from_hist(invm_cr_h),
-        production.radiative_acceptance.alic_2016_simps
-    )
-    model = simp.Parameters(**simp_parameters)
-    eff = np.full(mass.shape+z.centers.shape, np.nan)
-    diff_yield = np.full(mass.shape+eps2.shape+z.centers.shape, np.nan)
     max_signal_allowed = np.full(mass.shape+eps2.shape, np.nan)
     expected_signal = np.full(mass.shape+eps2.shape, 0.0)
     for i_mass, m in tqdm(enumerate(mass), total=len(mass)):
-        diff_yield[i_mass,...], eff[i_mass,...] = signal_yield(
-            mass = m,
-            eps2 = eps2,
-            z = z,
-            prompt_signal_yield_per_eps2 = (
-                total_prompt_signal_yield_per_eps2(m*model.mass_ratio_Ap_to_Vd)
-            ),
-            final_selection_counts_h = final_selection_counts_by_mass_h[m],
-            mean_energy_GeV = mean_energy_GeV_by_mass[m],
-            model = model
-        )
         yield_cdf_lut = construct_yield_cdf_lut(diff_yield[i_mass,...])
         expected_signal[i_mass,:] = np.sum(diff_yield[i_mass,...], axis=1)
         data_x = (
@@ -85,6 +64,47 @@ def exclusion_estimate(*,
         diff_yield = diff_yield,
         expected = expected_signal,
         max_allowed = max_signal_allowed,
+        signal_efficiency = eff
+    )
+
+
+def exclusion_estimate(*,
+    mass,
+    eps2,
+    z,
+    invm_cr_h,
+    final_selection_counts_by_mass_h,
+    mean_energy_GeV_by_mass,
+    data_z,
+    simp_parameters = {},
+    signal_yield_calculator = None
+):
+    total_prompt_signal_yield_per_eps2 =  production.from_calculators(
+        production.radiative_fraction.alic_2016_simps,
+        production.TridentDifferentialProduction.from_hist(invm_cr_h),
+        production.radiative_acceptance.alic_2016_simps
+    )
+    model = simp.Parameters(**simp_parameters)
+    eff = np.full(mass.shape+z.centers.shape, np.nan)
+    diff_yield = np.full(mass.shape+eps2.shape+z.centers.shape, np.nan)
+    for i_mass, m in tqdm(enumerate(mass), total=len(mass)):
+        diff_yield[i_mass,...], eff[i_mass,...] = signal_yield(
+            mass = m,
+            eps2 = eps2,
+            z = z,
+            prompt_signal_yield_per_eps2 = (
+                total_prompt_signal_yield_per_eps2(m*model.mass_ratio_Ap_to_Vd)
+            ),
+            final_selection_counts_h = final_selection_counts_by_mass_h[m],
+            mean_energy_GeV = mean_energy_GeV_by_mass[m],
+            model = model
+        )
+
+    return exclusion_estimate_from_diff_yield(
+        mass = mass,
+        eps2 = eps2,
+        z = z.centers,
+        diff_yield = diff_yield,
         signal_efficiency = eff
     )
 
