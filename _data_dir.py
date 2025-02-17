4a16debd51d9a6e7f512a09c8ae1f0f89368846a
tomeichlersmith <eichl008@umn.edu>
2025-02-17 09:15:29 -0800

    update directory deducing for new location

--- _data_dir.py
+++ _data_dir.py
@@ -2,11 +2,13 @@
 
 from pathlib import Path
 
+_pass_name = 'final-simps-pass'
+
 def get_data_dir():
     import socket
     hn = socket.getfqdn()
     if hn.endswith('slac.stanford.edu') or 'sdfiana' in hn:
-        return Path('/sdf/group/hps/user-data/eichl008/simp-l1l2/analysis')
+        return Path('/sdf/data/hps/physics2016/')
     elif hn.endswith('zebra01.spa.umn.edu'):
         return Path('/export/scratch/users/eichl008/hps/simp-l1l2/data')
     elif hn.endswith('zuko'):
@@ -23,7 +25,7 @@ def get_true_vd_z_file():
         return attempt
     
     # on S3DF, keep the file in the parent directory of the analysis tuples
-    attempt = get_data_dir() / '..' / 'true-vd-z-pre-readout.root'
+    attempt = get_data_dir() / 'mc' / _pass_name / 'true-vd-z-pre-readout.root'
     if attempt.is_file():
         return attempt
     
--- dask/_data_list.py
+++ dask/_data_list.py
@@ -1,15 +1,15 @@
 """list data in dask-ready way"""
 
-from .._data_dir import get_data_dir
+from .._data_dir import get_data_dir, _pass_name
 
 def _test_sample_list():
     data_d = get_data_dir()
     name_to_fp = [
-        ('simp100', data_d / 'mass_100_hadd-simp-beam.root'),
-        ('wab', data_d / 'wab-beam-hadd-100files_0.root'),
-        ('rad', data_d / 'rad_beam_pass4b_tuples_100files_0.root'),
-        ('tritrig', data_d / 'tritrig-beam-hadd-100files_0.root'),
-        ('data', data_d / 'data-physrun2016-pass4kf-recon-5.2.1-run-007800-part-0.root')
+        ('simp100', data_d / 'mc' / _pass_name / 'simps_beam' / 'analysis' / 'mass_100_hadd-simp-beam.root'),
+        ('wab', data_d / 'mc' / _pass_name / 'wab_beam' / 'analysis' / 'wab-beam-hadd-100files_0.root'),
+        ('rad', data_d / 'mc' / _pass_name / 'rad_beam' / 'analysis' / 'rad_beam_pass4b_tuples_100files_0.root'),
+        ('tritrig', data_d / 'mc' / _pass_name / 'tritrig_beam' / 'analysis' / 'tritrig-beam-hadd-100files_0.root'),
+        ('data', data_d / 'data' / _pass_name / 'physics' / 'analysis' / 'data-physrun2016-pass4kf-recon-5.2.1-run-007800-part-0.root')
     ]
     return [
         (name, {fp:'preselection'})
@@ -41,7 +41,7 @@ _data_filters = {
     'full-lumi': _full_lumi,
     '10pct' : _data_10pct,
     'golden-run': _golden_run,
-    'alic-10pct': _alic_10pct
+    #'alic-10pct': _alic_10pct # while historically helpful, not being carried forward
 }
 
 def _full_sample_list(data_filter = None):
@@ -50,25 +50,28 @@ def _full_sample_list(data_filter = None):
         data_filter = _is_data
     elif isinstance(data_filter, str):
         data_filter = _data_filters[data_filter]
-    from collections import defaultdict
-    samples = defaultdict(dict)
-    for fp in data_d.iterdir():
-        if fp.suffix != '.root' or fp.stem == 'true-vd-z-pre-readout':
-            continue
-        if _is_data(fp):
-            if data_filter(fp):
-                samples['data'][fp] = 'preselection'
-        elif fp.stem.endswith('simp-beam'):
-            m = fp.stem.split('_')[1]
-            samples[f'simp{m}'][fp] = 'preselection'
-        elif fp.stem.startswith('wab'):
-            samples['wab'][fp] = 'preselection'
-        elif fp.stem.startswith('rad'):
-            samples['rad'][fp] = 'preselection'
-        elif fp.stem.startswith('tritrig'):
-            samples['tritrig'][fp] = 'preselection'
-        else:
-            raise ValueError(fp)
+    samples = {
+        'data': {
+            fp : 'preselection'
+            for fp in (data_d / 'data' / _pass_name / 'physics' / 'analysis').iterdir()
+            if fp.suffix == '.root' and data_filter(fp)
+        }
+    }
+    samples.update({
+        bkgd : {
+            fp: 'preselection'
+            for fp in (data_d / 'mc' / _pass_name / f'{bkgd}_beam' / 'analysis').iterdir()
+            if fp.suffix == '.root'
+        }
+        for bkgd in ['wab','rad','tritrig']
+    })
+    samples.update({
+        f'simp{fp.stem.split("_")[1]}': {
+            fp: 'preselection'
+        }
+        for fp in (data_d / 'mc' / _pass_name / 'simps_beam' / 'analysis').iterdir()
+        if fp.suffix == '.root'
+    })
     return samples.items()
 
 
